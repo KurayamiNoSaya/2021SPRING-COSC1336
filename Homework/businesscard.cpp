4b9d240c225ee834ed3018fcdae461e11f5fad7d
Dulce Ocura <81393687+KurayamiNoSaya@users.noreply.github.com>
2021-03-30 20:01:36 -0500

    Add files via upload

---
+++ Homework/businesscard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std ::cout <<"Minnano Japanese Grocery" << std::endl;
+    std ::cout <<"7460 Callaghan Rd, San Antonio,TX 78229" << std::endl;
+    std ::cout <<"+12103407900" << std::endl;
+    std ::cout <<"https:m.facebook.com/minnano.japanesegrocery" << std::endl;
+    return 0;
+}
