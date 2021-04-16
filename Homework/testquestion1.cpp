c8de9f1e668ed06fdb0a67427b0fadacc94df3ab
Dulce Ocura <81393687+KurayamiNoSaya@users.noreply.github.com>
2021-04-15 19:14:41 -0500

    Add files via upload

---
+++ Homework/testquestion1.cpp
@@ -0,0 +1,14 @@
+//Dulce
+//Test Question 1
+
+#include <iostream>
+
+int main (){
+    std::cout << "People can sing" << std::endl;
+    std::cout << "People can swim." << std::endl;
+    std::cout << "People can play guitar." << std::endl;
+    std::cout << "People can paint portraits." << std::endl;
+    std::cout << "People can walk a dog." << std::endl;
+
+    return 0;
+}
